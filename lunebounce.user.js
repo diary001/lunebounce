7549c750a0e93e6bb46f5ec766e08d481225fca8
lune <138798718+Luna@users.noreply.github.com>
2024-05-22 14:56:38 -0400

    Update lunebounce.user.js

--- lunebounce.user.js
+++ lunebounce.user.js
@@ -4,7 +4,7 @@
 // @supportURL   https://discord.gg/mbe
 // @license      MIT
 // @version      1.0.9
-// @description  ((made with <3 by lune))
+// @description  This is the first theme for Moonbounce! I spent a few months working on this to make sure everything was perfected. This theme was created solely by me, so please do not message the Moonbonuce team for support. ((made with <3 by lune))
 // @author       lune / linktr.ee/moon
 // @match        *://*/*
 // @icon         https://framerusercontent.com/images/iuVZmsE2sifPExZXNrETQrDkU.png
